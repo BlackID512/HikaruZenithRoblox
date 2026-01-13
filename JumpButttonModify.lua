31018ed25d354fac33227687d7e6e01b5519d09e
BlackID512 <53506204+BlackID512@users.noreply.github.com>
2026-01-13 11:48:48 +0900

    Create JumpButttonModify.lua

---
+++ JumpButttonModify.lua
@@ -0,0 +1,2 @@
+getgenv().script = "https://raw.githubusercontent.com/BlackID512/HikaruZenithRoblox/master/JumpButtonModifier.lua"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlackID512/HikaruZenithRoblox/master/JumpButtonModifierLoad.lua"))()
