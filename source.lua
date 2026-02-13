e2b1cd2763d60817faf4daf0838e2c452e795f9d
BlackID512 <53506204+BlackID512@users.noreply.github.com>
2026-02-13 19:35:25 +0900

    Update source.lua

--- source.lua
+++ source.lua
@@ -13163,25 +13163,6 @@ addcmd("volumedown",{"voldown"}, function(args, speaker)
 	notify('Volume Down',notifyDesc)
 end)
 
-addcmd('testheal',{},function(args, speaker)
-	local plr = Players.LocalPlayer
-	local health = plr.Character:FindFirstChildOfClass("Humanoid").Health
-	local newHeal = 100
-	if tonumber(args[1]) > 0 then
-		newHeal = tonumber(args[1])
-		if tonumber(args[1]) > 100 then
-			newHeal = 100
-		elseif tonumber(args[1]) < 1 then
-			newHeal = 1
-		end
-		local notifyDescText = 'Current Health: ' .. health
-		local notifyDesc = tostring(notifyDescText)
-		notify('Test Heal',notifyDesc)
-	else
-		notify('Test Heal','Usage: testheal (1 - 100)')
-	end
-end)
-
 addcmd('testnow',{},function(args, speaker)
 	local now = Time()
 	local now2 = os.time()
@@ -13190,7 +13171,8 @@ addcmd('testnow',{},function(args, speaker)
 	notify('Test Time',desc)
 end)
 
-if IsOnMobile then
+-- if IsOnMobile then
+if currentVersion == "0.0.1" then
 	local QuickCapture = Instance.new("TextButton")
 	-- local QuickCapture = Instance.new("ImageButton")
 	local UICorner = Instance.new("UICorner")
